7204f3f374dcc47fca6b990944c64ddd750265bf
Arshad Khan <arshadkhan7867865422@gmail.com>
2023-07-04 00:29:47 +0530

    added dependencies

---
+++ lib/pages/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(child: Scaffold());
+  }
+}
